d0127ddb77d33fac77d8ac4e4c2688d35921018a
Abakar <skat131998@mail.ru>
2017-01-18 17:15:31 +0300

    MultiSpinner is added

--- CIT2/app/src/main/java/com/cit/abakar/application/EquipmentStateActivity.java
+++ CIT2/app/src/main/java/com/cit/abakar/application/EquipmentStateActivity.java
@@ -3,6 +3,7 @@ package com.cit.abakar.application;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,13 +13,15 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 
-public class EquipmentStateActivity extends Activity {
+
+public class EquipmentStateActivity extends Activity implements MultiSelectionSpinner.MultiSpinnerListener {
 
     Button button1, button2;
     Switch switch1, switch2;
-    //MultiSelectionSpinner spinner;
-    Spinner spinner;
+    MultiSelectionSpinner spinner;
+    ArrayList<String> ar = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +32,15 @@ public class EquipmentStateActivity extends Activity {
         button2 = (Button) findViewById(R.id.button2InEquipmentState);
         switch1 = (Switch) findViewById(R.id.switch1InEquipmentState);
         switch2 = (Switch) findViewById(R.id.switch2InEquipmentState);
-        spinner = (Spinner) findViewById(R.id.spinnerInEquipmentActivity);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.forSpinner, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-      //  spinner.setItems(getResources().getStringArray(R.array.forSpinner));
+        spinner = (MultiSelectionSpinner) findViewById(R.id.spinnerInEquipmentActivity);
+        ar.add("Сломан кулер");
+        ar.add("Не работает автофокус");
+        ar.add("Перегрев процессора");
+        ar.add("Другое");
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.forSpinner, android.R.layout.simple_spinner_item);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       // spinner.setAdapter(adapter);
+        spinner.setItems(ar,"Выбрать причину",this);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +52,7 @@ public class EquipmentStateActivity extends Activity {
                     public void onClick(View v) {
                         Toast toast = Toast.makeText(EquipmentStateActivity.this,spinner.getSelectedItem().toString(),Toast.LENGTH_SHORT);
                         toast.show();
+                        //Log.e("getSelected", s);
                         Intent intent = new Intent(EquipmentStateActivity.this, EquipmentActivity.class);
                         startActivity(intent);
                     }
@@ -58,5 +66,10 @@ public class EquipmentStateActivity extends Activity {
     public void onBackPressed() {
         Intent intent = new Intent(this, EquipmentActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemsSelected(boolean[] selected) {
+        Log.e("WTF", selected[0] + "");
     }
 }
--- CIT2/app/src/main/java/com/cit/abakar/application/MultiSelectionSpinner.java
+++ CIT2/app/src/main/java/com/cit/abakar/application/MultiSelectionSpinner.java
@@ -13,175 +13,98 @@ import java.util.LinkedList;
 import java.util.List;
 
 
-public class MultiSelectionSpinner extends Spinner implements DialogInterface.OnMultiChoiceClickListener {
-
-    String[] items = null;
-    boolean[] mSelection = null;
-
-    ArrayAdapter<String> simpleAdapter;
+public class MultiSelectionSpinner extends Spinner implements DialogInterface.OnMultiChoiceClickListener, DialogInterface.OnCancelListener {
+    private List<String> items;
+    private boolean[] selected;
+    private String defaultText;
+    private MultiSpinnerListener listener;
 
     public MultiSelectionSpinner(Context context) {
         super(context);
-
-        simpleAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item);
-        super.setAdapter(simpleAdapter);
     }
 
-    public MultiSelectionSpinner(Context context, AttributeSet attrs) {
-        super(context, attrs);
-
-        simpleAdapter = new ArrayAdapter<String>(context,
-                android.R.layout.simple_spinner_item);
-        super.setAdapter(simpleAdapter);
+    public MultiSelectionSpinner(Context arg0, AttributeSet arg1) {
+        super(arg0, arg1);
     }
 
+    public MultiSelectionSpinner(Context arg0, AttributeSet arg1, int arg2) {
+        super(arg0, arg1, arg2);
+    }
 
     @Override
     public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-
-        if(mSelection != null && which < mSelection.length){
-            mSelection[which] = isChecked;
-            simpleAdapter.clear();
-            simpleAdapter.add(buildSelectedItemString());
-        }else{
-            throw new IllegalArgumentException("Argument 'which' is out of bounds");
-        }
+        if (isChecked)
+            selected[which] = true;
+        else
+            selected[which] = false;
     }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        // refresh text on spinner
+        StringBuffer spinnerBuffer = new StringBuffer();
+        boolean someUnselected = false;
+        for (int i = 0; i < items.size(); i++) {
+            if (selected[i] == true) {
+                spinnerBuffer.append(items.get(i));
+                spinnerBuffer.append(", ");
+            } else {
+                someUnselected = true;
+            }
+        }
+        String spinnerText;
+        if (someUnselected) {
+            spinnerText = spinnerBuffer.toString();
+            if (spinnerText.length() > 2)
+                spinnerText = spinnerText.substring(0, spinnerText.length() - 2);
+        } else {
+            spinnerText = defaultText;
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_item,
+                new String[] { spinnerText });
+        setAdapter(adapter);
+        listener.onItemsSelected(selected);
+    }
+
     @Override
     public boolean performClick() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setMultiChoiceItems(items, mSelection, this);
+        builder.setMultiChoiceItems(
+                items.toArray(new CharSequence[items.size()]), selected, this);
+        builder.setPositiveButton(android.R.string.ok,
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        builder.setOnCancelListener(this);
         builder.show();
         return true;
     }
 
-    @Override
-    public void setAdapter(SpinnerAdapter adapter) {
-        throw new RuntimeException(
-                "setAdapter is not supported by MultiSelectSpinner.");
+    public void setItems(List<String> items, String allText,
+                         MultiSpinnerListener listener) {
+        this.items = items;
+        this.defaultText = allText;
+        this.listener = listener;
+
+        // all selected by default
+        selected = new boolean[items.size()];
+        for (int i = 0; i < selected.length; i++)
+            selected[i] = true;
+
+        // all text on the spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_item, new String[] { allText });
+        setAdapter(adapter);
     }
 
-    public void setItems(String[] items) {
-        items = items;
-        mSelection = new boolean[items.length];
-        simpleAdapter.clear();
-        simpleAdapter.add(items[0]);
-        Arrays.fill(mSelection, false);
+    public interface MultiSpinnerListener {
+        public void onItemsSelected(boolean[] selected);
     }
 
-    public void setItems(List<String> items) {
-        this.items = items.toArray(new String[items.size()]);
-        mSelection = new boolean[this.items.length];
-        simpleAdapter.clear();
-        simpleAdapter.add(this.items[0]);
-        Arrays.fill(mSelection, false);
-    }
 
-    public void setSelection(String[] selection) {
-        for (String cell : selection) {
-            for (int j = 0; j < items.length; ++j) {
-                if (items[j].equals(cell)) {
-                    mSelection[j] = true;
-                }
-            }
-        }
-    }
-
-    public void setSelection(List<String> selection) {
-        for (int i = 0; i < mSelection.length; i++) {
-            mSelection[i] = false;
-        }
-        for (String sel : selection) {
-            for (int j = 0; j < items.length; ++j) {
-                if (items[j].equals(sel)) {
-                    mSelection[j] = true;
-                }
-            }
-        }
-        simpleAdapter.clear();
-        simpleAdapter.add(buildSelectedItemString());
-    }
-
-    public void setSelection(int index) {
-        for (int i = 0; i < mSelection.length; i++) {
-            mSelection[i] = false;
-        }
-        if (index >= 0 && index < mSelection.length) {
-            mSelection[index] = true;
-        } else {
-            throw new IllegalArgumentException("Index " + index
-                    + " is out of bounds.");
-        }
-        simpleAdapter.clear();
-        simpleAdapter.add(buildSelectedItemString());
-    }
-
-    public void setSelection(int[] selectedIndicies) {
-        for (int i = 0; i < mSelection.length; i++) {
-            mSelection[i] = false;
-        }
-        for (int index : selectedIndicies) {
-            if (index >= 0 && index < mSelection.length) {
-                mSelection[index] = true;
-            } else {
-                throw new IllegalArgumentException("Index " + index
-                        + " is out of bounds.");
-            }
-        }
-        simpleAdapter.clear();
-        simpleAdapter.add(buildSelectedItemString());
-    }
-
-    public List<String> getSelectedStrings() {
-        List<String> selection = new LinkedList<String>();
-        for (int i = 0; i < items.length; ++i) {
-            if (mSelection[i]) {
-                selection.add(items[i]);
-            }
-        }
-        return selection;
-    }
-
-    public List<Integer> getSelectedIndicies() {
-        List<Integer> selection = new LinkedList<Integer>();
-        for (int i = 0; i < items.length; ++i) {
-            if (mSelection[i]) {
-                selection.add(i);
-            }
-        }
-        return selection;
-    }
-
-    private String buildSelectedItemString() {
-        StringBuilder sb = new StringBuilder();
-        boolean foundOne = false;
-
-        for (int i = 0; i < items.length; ++i) {
-            if (mSelection[i]) {
-                if (foundOne) {
-                    sb.append(", ");
-                }
-                foundOne = true;
-
-                sb.append(items[i]);
-            }
-        }
-        return sb.toString();
-    }
-
-    public String getSelectedItemsAsString() {
-        StringBuilder sb = new StringBuilder();
-        boolean foundOne = false;
-
-        for (int i = 0; i < items.length; ++i) {
-            if (mSelection[i]) {
-                if (foundOne) {
-                    sb.append(", ");
-                }
-                foundOne = true;
-                sb.append(items[i]);
-            }
-        }
-        return sb.toString();
-    }
 }
