2ef2aee5f1cef13502201fe2beef9c3887b92528
Abakar <skat131998@mail.ru>
2017-01-23 17:51:34 +0300

    log

--- CIT2/app/src/main/java/com/cit/abakar/application/MainActivity.java
+++ CIT2/app/src/main/java/com/cit/abakar/application/MainActivity.java
@@ -113,6 +113,7 @@ public class MainActivity extends Activity {
                 myMediaPlayer.start();
                 myMediaPlayer.setFree();
                 Intent intent = new Intent(MainActivity.this, EquipmentActivity.class);
+                Log.e("PZD", arr.get(position).getId().toString());
                 intent.putExtra("id", arr.get(position).getId());
                 startActivity(intent);
             }
