00ab777cda96af28eeb5a8c717ddd91c3cc24fda
Abakar <skat131998@mail.ru>
2017-02-06 14:56:43 +0300

    work with deinstallation of equipment

--- CIT2/app/src/main/java/com/cit/abakar/application/EquipmentActivity.java
+++ CIT2/app/src/main/java/com/cit/abakar/application/EquipmentActivity.java
@@ -426,21 +426,19 @@ public class EquipmentActivity extends Activity implements AdapterInterface, Mul
         restApi.getEquipment().enqueue(new Callback<List<Equipment>>() {
             @Override
             public void onResponse(Call<List<Equipment>> call, Response<List<Equipment>> response) {
-                for (Equipment eq : response.body()) {
-                    if (eq.centerId == getIntent().getIntExtra("id", -5)) {
-                        arr.add(eq);
-                    }
-                }
-                Equipment equipment = new Equipment();
-                equipment.id = 3;
-                equipment.centerId = 1;
-                equipment.serialNumber = "gvdfv";
-                equipment.inventoryNumber  = "32fdsfs";
-                equipment.name = "Очень длинное название оборудования";
-                equipment.fgDismantled="false";
-                equipment.fgNotInstall="false";
 
-                arr.add(equipment);
+                try {
+                    ArrayList<Equipment> equipment = (ArrayList<Equipment>)response.body();
+                    for (Equipment eq : response.body()) {
+                        if (eq.centerId == getIntent().getIntExtra("id", -5)) {
+                            arr.add(eq);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    arr.clear();
+                    buttonInspectionDone.setEnabled(true);
+                }
 
                 adapter = new MyAdapter(EquipmentActivity.this, arr, veryfied);
                 listView.setAdapter(adapter);
